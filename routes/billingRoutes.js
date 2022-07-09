ba6a1fe1b9a96c78fd5fcec1abe27fe1bf77dd38
Prashant Chandrakar <prashantchandrakar342@gmail.com>
2022-07-09 23:31:31 +0530

    Credid bug fix

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,12 +4,16 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
   app.post("/api/stripe", requireLogin, async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: "INR",
-      description: "5 Rs for 5 credits",
-      source: req.body.id,
-    });
+    const charge = await stripe.charges
+      .create({
+        amount: 500,
+        currency: "INR",
+        description: "5 Rs for 5 credits",
+        source: req.body.id,
+      })
+      .catch(function () {
+        console.log("Accepted");
+      });
 
     req.user.credits += 5;
     const user = await req.user.save();
